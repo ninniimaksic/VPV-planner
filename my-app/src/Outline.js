6acd651ec397edb7d11aea24c15ff51a27cc7688
HenrikEide <Henrik225@gmail.com>
2023-06-14 14:28:31 +0200

    less jank

--- my-app/src/Outline.js
+++ my-app/src/Outline.js
@@ -1,71 +1,65 @@
-// import the fabric library
+import React, { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 
 const Outline = (props) => {
-  var photo = props.selectedPhoto;
-  // initialize canvas
-  let canvas = new fabric.Canvas("canvas");
+  const canvasRef = useRef(null);
+  const { selectedPhoto } = props;
 
-  // load image
-  fabric.Image.fromURL(photo, function (img) {
-    // scale image if necessary
-    img.scale(1);
-    canvas.add(img);
-    canvas.renderAll();
-  });
-
-  // initialize array to store points
-  let roofPoints = [];
-  let isDrawing = false;
-
-  // add event listeners for mouse events
-  canvas.on("mouse:down", function (options) {
-    isDrawing = true;
-    let pointer = canvas.getPointer(options.e);
-    roofPoints.push({ x: pointer.x, y: pointer.y });
-  });
-
-  canvas.on("mouse:move", function (options) {
-    if (!isDrawing) return;
-    let pointer = canvas.getPointer(options.e);
-    roofPoints.push({ x: pointer.x, y: pointer.y });
-    canvas.renderAll();
-  });
-
-  canvas.on("mouse:up", function () {
-    isDrawing = false;
-
-    // create and add polygon to canvas when mouse is released
-    let roofOutline = new fabric.Polygon(roofPoints, {
-      stroke: "red",
-      fill: "rgba(0,0,0,0)",
-      strokeWidth: 2,
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      preserveObjectStacking: true,
     });
-    canvas.add(roofOutline);
-    canvas.renderAll();
 
-    // clear points
-    roofPoints = [];
-  });
+    fabric.Image.fromURL(selectedPhoto, function (img) {
+      img.scale(1);
+      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+    });
 
-  // calculate area function
-  function calculateArea(points) {
-    let area = 0;
-    for (let i = 0; i < points.length; i++) {
-      let j = (i + 1) % points.length;
+    let roofPoints = [];
+    let isDrawing = false;
 
-      area += points[i].x * points[j].y;
-      area -= points[j].x * points[i].y;
-    }
-    area /= 2;
-    return Math.abs(area); // returns in pixels
-  }
+    canvas.on("mouse:down", function (options) {
+      isDrawing = true;
+      let pointer = canvas.getPointer(options.e);
+      roofPoints.push({ x: pointer.x, y: pointer.y });
+    });
 
-  // use the calculateArea function
-  let area = calculateArea(roofPoints);
-  console.log(area);
+    canvas.on("mouse:move", function (options) {
+      if (!isDrawing) return;
+      let pointer = canvas.getPointer(options.e);
+      roofPoints.push({ x: pointer.x, y: pointer.y });
+      canvas.renderAll();
+    });
 
-  return <canvas id="canvas" width="500" height="500" />;
+    canvas.on("mouse:up", function () {
+      isDrawing = false;
+
+      let roofOutline = new fabric.Polygon(roofPoints, {
+        stroke: "red",
+        fill: "rgba(0,0,0,0)",
+        strokeWidth: 2,
+      });
+      canvas.add(roofOutline);
+      canvas.renderAll();
+
+      roofPoints = [];
+    });
+
+    return () => {
+      canvas.dispose();
+    };
+  }, [selectedPhoto]);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <canvas
+        ref={canvasRef}
+        style={{ border: "1px solid black" }}
+        width={800}
+        height={600}
+      />
+    </div>
+  );
 };
 
 export default Outline;
