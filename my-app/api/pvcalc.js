33fa4e28fcc1e78447e59044b36fc4f8ac92412c
HenrikEide <Henrik225@gmail.com>
2023-08-01 15:49:23 +0200

    api serverless func added

---
+++ my-app/api/pvcalc.js
@@ -0,0 +1,29 @@
+// api/pvcalc.js
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { lat, lon, azimuth, peakpower, loss } = req.query;
+
+  try {
+    const response = await axios.get(
+      `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc`,
+      {
+        params: {
+          outputformat: "json",
+          lat,
+          lon,
+          peakpower,
+          loss,
+          angle: 90,
+          aspect: azimuth,
+        },
+      }
+    );
+
+    // Return the API response data to the client
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("API request failed:", error);
+    res.status(500).json({ error: "API request failed" });
+  }
+}
