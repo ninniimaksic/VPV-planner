1947a86d32589db974a4b8b3bdc9e326effe378d
HenrikEide <Henrik225@gmail.com>
2023-08-07 14:54:06 +0200

    aspect always between -180 and 180

--- my-app/api/pvcalc.js
+++ my-app/api/pvcalc.js
@@ -1,11 +1,19 @@
 export default async function handler(req, res) {
   const { lat, lon, azimuth, peakpower, loss } = req.query;
 
+  const aspectCalc = (azimuth) => {
+    let aspect = (parseInt(azimuth) + 180) % 360;
+    if (aspect > 180) {
+      aspect = aspect - 360;
+    }
+    return aspect;
+  };
+
   try {
     const response = await fetch(
-      `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?outputformat=json&lat=${lat}&lon=${lon}&peakpower=${peakpower}&loss=${loss}&angle=90&aspect=${
-        (parseInt(azimuth) + 180) % 360
-      }`
+      `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?outputformat=json&lat=${lat}&lon=${lon}&peakpower=${peakpower}&loss=${loss}&angle=90&aspect=${aspectCalc(
+        azimuth
+      )}`
     );
 
     if (response.ok) {
