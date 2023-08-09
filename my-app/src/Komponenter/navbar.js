f832b9eb0d33fd3ac5d1ccab5aba42260e1fb570
nikolim <ninni@Nikolijas-MBP-3.var-lan.local>
2023-08-09 15:57:08 +0200

    Home is where your 🫶🏼 is

--- my-app/src/Komponenter/navbar.js
+++ my-app/src/Komponenter/navbar.js
@@ -3,6 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import "../css/navbar.css";
 import logo1 from "../img/logo1.png";
 import { LeaveIcon } from "@navikt/aksel-icons";
+import { HouseHeartIcon } from "@navikt/aksel-icons";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
@@ -34,21 +35,24 @@ const Navbar = () => {
           {" - "}
           {sessionStorage.getItem("projectNumber")}
         </h2>
-        <div>
-          <ul id="navbar" className={clicked ? "navbar active" : "navbar"}>
-            <li style={{ display: "flex", alignItems: "center" }}>
-              <span> Projects</span>
 
-              <Link to="/" onClick={handleLeaveIconClick}>
-                <LeaveIcon
-                  title="a11y-title"
-                  fontSize="1.8rem"
-                  color="white"
-                  className="leave-icon-hover"
-                />
-              </Link>
-            </li>
-          </ul>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Link to="/home" className="icon-hover">
+            <HouseHeartIcon
+              title="a11y-title"
+              fontSize="1.8rem"
+              color="white"
+            />
+          </Link>
+
+          <Link to="/" onClick={handleLeaveIconClick} className="no-hover-line">
+            <LeaveIcon
+              title="a11y-title"
+              fontSize="1.8rem"
+              color="white"
+              className="leave-icon-hover"
+            />
+          </Link>
         </div>
 
         <div id="mobile" onClick={handleMobileIconClick}>
